4cc754c303e09a10ec935e1896bcaa40c86527b2
Bolo Michelin <>
2022-06-22 15:32:35 -0400

    update varieties

--- db/migrate/20220622180200_create_varieties.rb
+++ db/migrate/20220622180200_create_varieties.rb
@@ -5,12 +5,14 @@ class CreateVarieties < ActiveRecord::Migration[7.0]
       t.integer :name_published_in_year
       t.string :taxon_remarks
       t.string :nomen_clatural_status
+
       t.string :locations, array: true
+      t.string :authors, array: true
+
       t.boolean :synonym
       t.boolean :plantae
       t.boolean :fungi
       t.string  :fq_id
-      t.string  :name
       t.string  :name
 
       t.timestamps
